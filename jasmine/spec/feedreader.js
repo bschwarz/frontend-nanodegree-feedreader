d294a295ae58bd80c0d29968011dc82e9edf33c1
Brett Schwarz <brett.schwarz@gmail.com>
2018-05-08 09:40:54 -0700

    added test suite to test the initial entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -80,8 +80,10 @@ $(function() {
             expect($('body').hasClass('menu-hidden')).toBeTruthy();
         });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    });
 
+    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
